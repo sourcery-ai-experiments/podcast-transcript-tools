2922bfb5fed0f1bd29a4979ab6ed54088c716900
Harold Martin <harold.martin@gmail.com>
2024-06-16 16:38:47 -0700

    Add Apache License and convert script for podcast transcripts

---
+++ podcast_transcript_tools/json_to_json.py
@@ -0,0 +1,17 @@
+import json
+from pathlib import Path
+
+from loguru import logger
+
+
+def json_file_to_json_file(origin_file: str, destination_file: str) -> None:
+
+    data = json.loads(Path(origin_file).read_text())
+    if "version" not in data or "segments" not in data:
+        logger.warning(f"Non-spec JSON file: {origin_file}")
+    Path(destination_file).write_text(
+        data=json.dumps(
+            data,
+            indent=4,
+        ),
+    )
