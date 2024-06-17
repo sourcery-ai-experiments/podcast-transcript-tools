c7ddcc016f11645ac1bea2aa728252b19e06ab5b
Harold Martin <harold.martin@gmail.com>
2024-06-17 16:47:41 -0700

    invalid SRT tests

--- tests/test_srt.py
+++ tests/test_srt.py
@@ -2,12 +2,14 @@ from pathlib import Path
 
 import pytest
 
+from podcast_transcript_tools.errors import InvalidSrtError
 from podcast_transcript_tools.srt_to_json import _ts_to_secs, srt_to_podcast_dict
 
 
 def test__ts_to_secs():
     assert _ts_to_secs("00:00:00,0") == 0
     assert _ts_to_secs("00:01:31,123") == 91.123
+    assert _ts_to_secs("23:59:59,999") == 86399.999
     assert _ts_to_secs("02:01:31,0") == 7291
     with pytest.raises(ValueError):
         _ts_to_secs("abc")
@@ -30,3 +32,8 @@ def test_srt_to_podcast_dict():
     assert transcript_dict["segments"][-1]["body"] == "Take care, Chelsea."
     assert transcript_dict["segments"][-1]["startTime"] == 2173.940
     assert transcript_dict["segments"][-1]["endTime"] == 2174.920
+
+
+def test_srt_to_podcast_dict_invalid():
+    with pytest.raises(InvalidSrtError):
+        srt_to_podcast_dict("whatever")
