2a1c7b8da6fe6c60b8a9470fe6d2cfe518e1a63c
Harold Martin <harold.martin@gmail.com>
2024-06-18 14:06:10 -0700

    WebVTT conversion

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # podcast-transcript-tools
 
+[![Lint and Test](https://github.com/hbmartin/podcast-transcript-tools/actions/workflows/lint.yml/badge.svg)](https://github.com/hbmartin/podcast-transcript-tools/actions/workflows/lint.yml)
+[![Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
+[![Code style: black](https://img.shields.io/badge/🐧️-black-000000.svg)](https://github.com/psf/black)
+[![Checked with pytype](https://img.shields.io/badge/🦆-pytype-437f30.svg)](https://google.github.io/pytype/)
+[![twitter](https://img.shields.io/badge/@hmartin-00aced.svg?logo=twitter&logoColor=black)](https://twitter.com/hmartin)
+
 Implementing indexing and conversion of podcast transcripts.
 
 https://github.com/Podcastindex-org/podcast-namespace/blob/main/transcripts/transcripts.md
@@ -18,3 +24,7 @@ pip install -r requirements.txt
 # Replace with the actual path to your transcript files
 python podcast_transcript_tools/convert.py ~/overcast-to-sqlite/archive/fixtures
 ```
+
+## Authors
+
+- [Harold Martin](https://www.linkedin.com/in/harold-martin-98526971/) - harold.martin at gmail
--- podcast_transcript_tools/vtt_to_json.py
+++ podcast_transcript_tools/vtt_to_json.py
@@ -1,19 +1,35 @@
-import json
+from json import dumps
 from pathlib import Path
 
 import webvtt  # type: ignore[import-not-found]
+from webvtt.errors import MalformedFileError
+
+from podcast_transcript_tools.errors import InvalidVttError
+from podcast_transcript_tools.srt_to_json import _mts_to_secs_float
 
 
-def vtt_to_podcast_dict(vtt: webvtt.WebVTT) -> dict:
-    # TODO: check timestamp types
+def vtt_to_podcast_dict(vtt_string: str) -> dict:
+    try:
+        vtt = webvtt.from_string(vtt_string)
+    except MalformedFileError as e:
+        raise InvalidVttError from e
     return {
         "version": "1.0.0",
         "segments": [
-            {
-                "startTime": caption.start,
-                "endTime": caption.end,
-                "body": caption.text,
-            }
+            (
+                {
+                    "startTime": _mts_to_secs_float(caption.start),
+                    "endTime": _mts_to_secs_float(caption.end),
+                    "body": caption.text.strip().replace("\n", " "),
+                    "speaker": caption.voice,
+                }
+                if caption.voice
+                else {
+                    "startTime": _mts_to_secs_float(caption.start),
+                    "endTime": _mts_to_secs_float(caption.end),
+                    "body": caption.text.strip().replace("\n", " "),
+                }
+            )
             for caption in vtt.captions
         ],
     }
@@ -21,11 +37,12 @@ def vtt_to_podcast_dict(vtt: webvtt.WebVTT) -> dict:
 
 # https://www.w3.org/TR/webvtt1/#file-structure
 def vtt_file_to_json_file(vtt_file: str, json_file: str) -> None:
+    vtt_string = Path(vtt_file).read_text()
+    try:
+        transcript_dict = vtt_to_podcast_dict(vtt_string)
+    except InvalidVttError as e:
+        e.add_note(vtt_file)
+        raise
     Path(json_file).write_text(
-        data=json.dumps(
-            vtt_to_podcast_dict(
-                webvtt.read(vtt_file),
-            ),
-            indent=4,
-        ),
+        data=dumps(transcript_dict, indent=4),
     )
